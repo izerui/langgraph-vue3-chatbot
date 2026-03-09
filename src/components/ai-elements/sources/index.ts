53b15e943b80837ba8976b6fb966dd948b19cb46
serv <liuyuhua69@gmail.com>
2026-03-09 14:31:13 +0800

    feat(ai-elements): 移除代码块组件及相关功能
    - 删除了 CodeBlock 组件及其相关子组件
    - 移除了代码高亮功能实现
    - 清理了相关的上下文和工具函数
    - 删除了聊天机器人示例中的代码块演示
    - 移除了队列组件相关文件

--- src/components/ai-elements/sources/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Source } from './Source.vue'
-export { default as Sources } from './Sources.vue'
-export { default as SourcesContent } from './SourcesContent.vue'
-export { default as SourcesTrigger } from './SourcesTrigger.vue'
